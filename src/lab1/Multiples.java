34f20bfbc8ca0f33c41d83e4e44e5c3babc939f6
Angeline <angelinehowcl@gmail.com>
2025-09-08 22:18:06 -0400

    added solution for multiples q

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int i;
+        int counter = 0;
+        for (i = 1; i < n; i++) {
+
+            if ((i % a == 0) || (i % b == 0)) {
+                counter+=1;
+            }
+
+        }
+        return counter;
+    }
+}
